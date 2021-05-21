eee82c26e5bd2f34353437eb7c67330cc1ca5af7
Matt Mead <mattmead@matts-mbp.lan>
2021-05-20 22:11:56 -0500

    Updated projects page and launching

---
+++ static/js/projects.js
@@ -0,0 +1,23 @@
+console.log("projects.js running");
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    boxes.forEach(box => {
+
+        //getBoundingClientRect finds attributes of element, including top, bottom, x and y coordinates
+        const boxTop = box.getBoundingClientRect().top;
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show');
+        }
+    })
+}
