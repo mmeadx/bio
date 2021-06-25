514a861ba57a711d261df9dd64e10e282eff5f69
Matt Mead <mattmead@Matts-MacBook-Pro.local>
2021-06-25 13:47:43 -0500

    updated projects page

---
+++ static/js/expandingCards.js
@@ -0,0 +1,16 @@
+console.log("script.js running");
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
