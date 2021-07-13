02950f42d165899d57af9697588e031b76a5742b
mmead <mmeadx@gmail.com>
2021-07-13 12:28:24 -0500

    updated skills

--- static/js/skills.js
+++ static/js/skills.js
@@ -2,7 +2,7 @@ console.log("skills.js running");
 
 const skillBox = document.getElementById('skillBox');
 
-const skills = ["JavaScript", "Python", "HTML5", "CSS3", "Node.js", "React.js", "DOM Manipulation", "npm", "Express.js", "jQuery", "d3.js", "EJS", "RESTful API", "Leaflet & GIS", "MatPlotLib", "Numpy", "Pandas", "jupyter notebook", "SQLAlchemy", "Mongoose", "ETL Processes", "Git", "GitHub", "Tableau", "Heroku", "Bootstrap", "Advanced Excel", "Wordpress", "Big Data", "R", "Codepen.io", "VS Code", "Atom", "Adobe Creative Suite", "MongoDB", "SQL", "postgreSQL", "SQLite"]
+const skills = ["JavaScript", "Python", "Node.js", "React.js", "DOM Manipulation", "npm", "Express.js", "jQuery", "d3.js", "EJS", "RESTful API", "Leaflet & GIS", "MatPlotLib", "Numpy", "Pandas", "jupyter notebook", "SQLAlchemy", "Mongoose", "ETL Processes", "Git", "GitHub", "Tableau", "Heroku", "Bootstrap", "Advanced Excel", "Wordpress", "Big Data", "R", "Codepen.io", "VS Code", "Atom", "Adobe Creative Suite", "MongoDB", "SQL", "postgreSQL", "SQLite", "HTML5", "CSS3"]
 
 function showSkills(skillList) {
     
